256762263a6e0238f919d95204104f5a45243890
R0rt1z2 <rogerortizleal@gmail.com>
2024-01-22 13:10:24 +0100

    fleur: Hexpatch camera lib to increase maximum JPEG streams for an app
    * Increase maxAppJpegStreamNum to 17 in order to fix GCam lags.
    
    Change-Id: I190a2c39e406e0a8389a7bf29b420ef4f2352e2d
    Signed-off-by: claxten10 <claxten10@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -115,6 +115,10 @@ function blob_fixup {
             [ "$2" = "" ] && return 0
             "${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "${2}"
             ;;
+        vendor/lib64/libmtkcam_featurepolicy.so)
+            # evaluateCaptureConfiguration()
+            sed -i "s/\x34\xE8\x87\x40\xB9/\x34\x28\x02\x80\x52/" "$2"
+            ;;
         vendor/bin/hw/android.hardware.gnss-service.mediatek |\
         vendor/lib64/hw/android.hardware.gnss-impl-mediatek.so)
             [ "$2" = "" ] && return 0
