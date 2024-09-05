7cd8aab981f95a27c037c7685710d69566090c9c
Michael Bestas <mkbestas@lineageos.org>
2024-09-05 19:46:51 +0100

    fleur: Sync extract scripts with templates
    Change-Id: I98a36854c5a0c0390592911bb2558b34eff77efc

--- extract-files.sh
+++ extract-files.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 #
-# Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017-2023 The LineageOS Project
-#
+# SPDX-FileCopyrightText: 2016 The CyanogenMod Project
+# SPDX-FileCopyrightText: 2017-2024 The LineageOS Project
 # SPDX-License-Identifier: Apache-2.0
 #
 
@@ -18,6 +17,10 @@ if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 ANDROID_ROOT="${MY_DIR}/../../.."
 
 export TARGET_ENABLE_CHECKELF=true
+
+# If XML files don't have comments before the XML header, use this flag
+# Can still be used with broken XML files by using blob_fixup
+export TARGET_DISABLE_XML_FIXING=true
 
 HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
@@ -35,22 +38,23 @@ SECTION=
 
 while [ "${#}" -gt 0 ]; do
     case "${1}" in
-        --only-firmware )
-                ONLY_FIRMWARE=true
-                ;;
-        -n | --no-cleanup )
-                CLEAN_VENDOR=false
-                ;;
-        -k | --kang )
-                KANG="--kang"
-                ;;
-        -s | --section )
-                SECTION="${2}"; shift
-                CLEAN_VENDOR=false
-                ;;
-        * )
-                SRC="${1}"
-                ;;
+        --only-firmware)
+            ONLY_FIRMWARE=true
+            ;;
+        -n | --no-cleanup)
+            CLEAN_VENDOR=false
+            ;;
+        -k | --kang)
+            KANG="--kang"
+            ;;
+        -s | --section)
+            SECTION="${2}"
+            shift
+            CLEAN_VENDOR=false
+            ;;
+        *)
+            SRC="${1}"
+            ;;
     esac
     shift
 done
@@ -62,43 +66,55 @@ fi
 function blob_fixup {
     case "$1" in
         system_ext/priv-app/ImsService/ImsService.apk)
+            [ "$2" = "" ] && return 0
             apktool_patch "${2}" "${MY_DIR}/blob-patches/ImsService.patch" -r
             ;;
         system_ext/lib64/libsink.so)
+            [ "$2" = "" ] && return 0
             "${PATCHELF}" --add-needed "libshim_sink.so" "$2"
             ;;
 	system_ext/lib64/libsource.so)
+            [ "$2" = "" ] && return 0
             grep -q "libui_shim.so" "${2}" || "${PATCHELF}" --add-needed "libui_shim.so" "${2}"
             ;;
 	vendor/etc/init/android.hardware.neuralnetworks@1.3-service-mtk-neuron.rc)
+            [ "$2" = "" ] && return 0
             sed -i 's/start/enable/' "$2"
             ;;
         vendor/etc/init/android.hardware.secure_element@1.2-service-mediatek.rc)
+            [ "$2" = "" ] && return 0
             sed -i 's/sea/fleur/' "$2"
             ;;
         vendor/etc/init/android.hardware.bluetooth@1.1-service-mediatek.rc)
+            [ "$2" = "" ] && return 0
             sed -i '/vts/Q' "$2"
             ;;
 	vendor/etc/init/init.batterysecret.rc)
+            [ "$2" = "" ] && return 0
             sed -i '/seclabel/d' "$2"
 	    ;;
 	vendor/etc/init/init.mi_thermald.rc)
+            [ "$2" = "" ] && return 0
             sed -i '/seclabel/d' "$2"
 	    ;;
 	vendor/etc/camera/camerabooster.json)
+            [ "$2" = "" ] && return 0
             sed -i 's/"sea"/"fleur"/' "$2"
             ;;
         vendor/lib64/libwifi-hal-mtk.so)
+            [ "$2" = "" ] && return 0
             "$PATCHELF" --set-soname libwifi-hal-mtk.so "${2}"
             ;;
         vendor/bin/mnld|\
         vendor/lib*/libcam.utils.sensorprovider.so|\
         vendor/lib*/librgbwlightsensor.so|\
         vendor/lib*/libaalservice.so)
+            [ "$2" = "" ] && return 0
             "$PATCHELF" --add-needed "libshim_sensors.so" "$2"
             ;;
         vendor/lib64/libmtkcam_stdutils.so|\
         vendor/lib64/hw/android.hardware.camera.provider@2.6-impl-mediatek.so)
+            [ "$2" = "" ] && return 0
             "${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "${2}"
             ;;
         vendor/lib64/libmtkcam_featurepolicy.so)
@@ -108,25 +124,39 @@ function blob_fixup {
             ;;
         vendor/bin/hw/android.hardware.gnss-service.mediatek |\
         vendor/lib64/hw/android.hardware.gnss-impl-mediatek.so)
+            [ "$2" = "" ] && return 0
             "$PATCHELF" --replace-needed "android.hardware.gnss-V1-ndk_platform.so" "android.hardware.gnss-V1-ndk.so" "$2"
             ;;
         vendor/bin/hw/android.hardware.media.c2@1.2-mediatek)
+            [ "$2" = "" ] && return 0
             "${PATCHELF}" --add-needed "libstagefright_foundation-v33.so" "${2}"
             "${PATCHELF}" --replace-needed "libavservices_minijail_vendor.so" "libavservices_minijail.so" "${2}"
             "${PATCHELF}" --set-soname "${2}" "${2}"
             ;;
         vendor/lib*/hw/vendor.mediatek.hardware.pq@2.15-impl.so)
+            [ "$2" = "" ] && return 0
             "$PATCHELF" --replace-needed "libutils.so" "libutils-v32.so" "$2"
             ;;
         vendor/lib64/vendor.mediatek.hardware.power@1.1.so|\
         vendor/lib64/vendor.mediatek.hardware.power@2.0.so)
+            [ "$2" = "" ] && return 0
             "${PATCHELF}" --add-needed "libshim_power.so" "${2}"
             ;;
         vendor/lib*/libwvhidl.so|\
         vendor/lib*/mediadrm/libwvdrmengine.so)
+            [ "$2" = "" ] && return 0
             "${PATCHELF}" --replace-needed "libprotobuf-cpp-lite-3.9.1.so" "libprotobuf-cpp-full-3.9.1.so" "${2}"
             ;;
+        *)
+            return 1
+            ;;
     esac
+
+    return 0
+}
+
+function blob_fixup_dry() {
+    blob_fixup "${1}" ""
 }
 
 # Initialize the helper
