dc7f3708a3e38a94cde6577b42d7433aa2e9cb86
Albert I <kras@raphielgang.org>
2024-10-23 20:51:33 +0000

    fleur: Inherit several Android Go configurations
    Beneficial for everyone, even on non-Go devices.
    
    Change-Id: I088fd00500ce10df5f413cb762aaed2ec00f5dfc
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>

--- device.mk
+++ device.mk
@@ -92,6 +92,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.1.vendor:64
 
+# Boot Image profile
+PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.device@3.6.vendor:64 \
