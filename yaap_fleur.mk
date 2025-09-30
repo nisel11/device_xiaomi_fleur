eb3d01fa4d646cc7d36e4a36ca8264f9d8ff7437
Nisel <nisel11good@gmail.com>
2025-09-30 18:41:20 +0300

    fleur: Add persist.sys.activity_anim_perf_override for fix settings lag
    Requires https://github.com/AxionAOSP/android_frameworks_base/commit/f89e8fa592233d86ad2cabf81df245c4003587cb in frameworks/base

--- yaap_fleur.mk
+++ yaap_fleur.mk
@@ -27,6 +27,9 @@ PRODUCT_SYSTEM_DEVICE := fleur
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.sys.activity_anim_perf_override=true
+
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="fleur_global-user 13 SP1A.210812.016 V816.0.9.0.TKEMIXM release-keys" \
     BuildFingerprint=Redmi/fleur_global/fleur:13/SP1A.210812.016/V816.0.9.0.TKEMIXM:user/release-keys \
